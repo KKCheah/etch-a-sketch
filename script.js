4a2007d4811a136e1e67b7f8a77dcee0fe23eb19
Cheah Kar Keong <ckarkeong@gmail.com>
2021-06-16 12:36:28 +0800

    Added prompt for user input and functions
    The prompt is added for user to insert a value whereas the functions is for the formation of grid within a container. The div are added into the container based on value inserted by the user. The console log shows that the function is working. Create element for div will be added
    
    [Ticket: #3 Date: 16/6/2021]

---
+++ script.js
@@ -0,0 +1,37 @@
+let container = document.querySelector('#container')
+let grid = document.createElement('div')
+
+function requestUserInput(){
+
+
+let numberOfGrid = prompt("How many boxes would you like?(key in a value within 1-64)");
+numberOfGrid = parseInt(numberOfGrid);
+
+if (numberOfGrid <4 || numberOfGrid>64){
+    alert("Value inserted not within range, please insert a value within 1 to 64");
+    requestUserInput();
+} else if (typeof numberOfGrid != "number" ) {
+    alert("Value inserted not a number, please insert a value within 1 to 64");
+    requestUserInput();
+} else {
+    console.log("reached end of requestUserInput function")
+    
+}
+    console.log("testing return");
+    return parseInt(numberOfGrid);
+}
+
+function gridFormation() {
+let numberOfGrid = requestUserInput();
+for (i = 0; i < numberOfGrid; i++){
+    container.appendChild(grid);
+    console.log("grid formation number" + i);
+}
+
+}
+
+let buttonCLick = document.getElementById('btn').addEventListener('click', () => {
+console.log("button test clicked");
+gridFormation();
+});
+
