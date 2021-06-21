16450eca6776f91f3804238a01b0f459e5319df8
Cheah Kar Keong <ckarkeong@gmail.com>
2021-06-21 22:05:31 +0800

    Added function that changes colour and opacity of grid
    Darkening effect whereby grid turns from grey to black has been added.Changed aesthetics of buttons. Project is presumably completed according to TOP curriculum with some extras. Some refinement/refactoring would be appropiate
    
    [Ticket: #8 Date: 21/6/2021]

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function gridFormation() {
 let numberOfGrid = requestUserInput();
 
     printOneRow(numberOfGrid*numberOfGrid);
-
+    showNoPixel(numberOfGrid);
 }
 
 
@@ -58,18 +58,20 @@ function printOneRow(numberOfGrid){
 
 
 
-let buttonCLick = document.getElementById('btn').addEventListener('click', () => {
+let buttonClick = document.getElementById('btn').addEventListener('click', () => {
 console.log("button test clicked");
 gridFormation();
+
 //activateAfter(i);
 });
-
-function activateAfter(i){
 
 function randomColour() {
     let randomiser = Math.floor(Math.random()*16777215).toString(16);  
     return randomiser;
 }
+
+function activateAfter(i){
+
 
 document.getElementById(`box_${i}`).addEventListener('mouseover', (e)=>{
     console.log(e);
@@ -82,7 +84,74 @@ document.getElementById(`box_${i}`).addEventListener('mouseout', (e)=>{
     document.getElementById(`box_${i}`).style.backgroundColor = "#" + randomColour(); 
     
 });
+
+document.getElementById('black').addEventListener('click',()=>{
+    console.log("black test clicked");
+    blackColour(i);
+})
+
+document.getElementById('multiColor').addEventListener('click',()=>{
+    console.log("multicolor test clicked");
+    randomColourButton(i); 
+})
+
+document.getElementById('erase').addEventListener('click',()=>{
+    console.log("erase test clicked");
+    erase(i);
+})
+
+document.getElementById('darker').addEventListener('click',()=>{
+    console.log("darker test clicked");
+    darkerColour(i);
+})
+
+
 }
+
+function incrementOpacity(x=0) {
+    x+=3;
+    return x
+}
+
+function darkerColour(i){
+    document.getElementById(`box_${i}`).addEventListener('mouseover', (e)=>{
+        console.log(e);
+        document.getElementById(`box_${i}`).style.backgroundColor = "black";
+        document.getElementById(`box_${i}`).style.opacity = (parseFloat(document.getElementById(`box_${i}`).style.opacity) || 0) + 0.1;
+        
+            setInterval(function(){
+                document.getElementById(`box_${i}`).style.opacity = (parseFloat(document.getElementById(`box_${i}`).style.opacity) || 0) + 0.1;
+            },1000)
+    });
+    
+    document.getElementById(`box_${i}`).addEventListener('mouseout', (e)=>{
+        console.log(e);
+        //document.getElementById(`box_${i}`).style.filter =  `brightness(${value}%)`;
+        document.getElementById(`box_${i}`).style.backgroundColor = "black";
+        setInterval(function(){
+            document.getElementById(`box_${i}`).style.opacity = (parseFloat(document.getElementById(`box_${i}`).style.opacity) || 0) - 0.05;
+        },900)
+        
+    });
+}
+
+
+function randomColourButton(i){
+    document.getElementById(`box_${i}`).addEventListener('mouseover', (e)=>{
+        console.log(e);
+        document.getElementById(`box_${i}`).style.backgroundColor = "#" + randomColour(); 
+        
+    });
+    
+    document.getElementById(`box_${i}`).addEventListener('mouseout', (e)=>{
+        console.log(e);
+        document.getElementById(`box_${i}`).style.backgroundColor = "#" + randomColour(); 
+        
+    });
+}
+
+
+
 
 function clearHTML() {
     while (container.hasChildNodes()){
@@ -99,3 +168,43 @@ document.getElementById('reset').addEventListener('click', () => {
     console.log("test reset");
     gridFormation();   
    })
+
+function showNoPixel(i){
+    let display = document.querySelector('#displayGridItem');
+    display.textContent = "The amount of pixel is now: " + i;
+    let displayContainer = document.querySelector('#displayGridNo');
+    displayContainer.appendChild(display);
+}
+
+//for black and white square:
+
+
+function blackColour(i) {
+    
+document.getElementById(`box_${i}`).addEventListener('mouseover', (e)=>{
+    console.log(e);
+    document.getElementById(`box_${i}`).style.backgroundColor = "black"; 
+    
+});
+
+document.getElementById(`box_${i}`).addEventListener('mouseout', (e)=>{
+    console.log(e);
+    document.getElementById(`box_${i}`).style.backgroundColor = "black"; 
+    
+});
+}
+
+function erase(i) {
+    
+    document.getElementById(`box_${i}`).addEventListener('mouseover', (e)=>{
+        console.log(e);
+        document.getElementById(`box_${i}`).style.backgroundColor = "white"; 
+        
+    });
+    
+    document.getElementById(`box_${i}`).addEventListener('mouseout', (e)=>{
+        console.log(e);
+        document.getElementById(`box_${i}`).style.backgroundColor = "white"; 
+        
+    });
+    }
